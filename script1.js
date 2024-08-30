736635c5e8ae54c4a6b626f47eee66d3fde39d90
abhi-r15 <abhi.r1509.git@gmail.com>
2024-08-31 00:16:49 +0530

    Add files via upload

--- script1.js
+++ script1.js
@@ -1,10 +1,10 @@
-function checkPasscode(event) {
-    event.preventDefault();  // Prevents the form from submitting and refreshing the page
-    const input = document.getElementById("secretInput").value;
-    const secret = "25qwerty";  // The correct string
-    if (input === secret) {
-        window.location.href = "2ndpage.html";  // The URL of the next page
-    } else {
-        alert("Incorrect response");
-    }
-}
+// function checkPasscode(event) {
+//     event.preventDefault();  // Prevents the form from submitting and refreshing the page
+//     const input = document.getElementById("secretInput").value;
+//     const secret = "25qwerty";  // The correct string
+//     if (input === secret) {
+//         window.location.href = "2ndpage.html";  // The URL of the next page
+//     } else {
+//         alert("Incorrect response");
+//     }
+// }
